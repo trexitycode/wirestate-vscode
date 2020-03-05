79fdb98515a41f94b810e48842c3d1a664c86b19
Mathieu Bouchard <mbouchard@launchfort.com>
2020-03-05 09:40:28 -0500

    fix scrollIntoView helper so it supports horizontal scrolling

--- vendor/scrollIntoViewIfOutOfView.js
+++ vendor/scrollIntoViewIfOutOfView.js
@@ -3,17 +3,25 @@
 function scrollIntoViewIfOutOfView (el, options) {
   var topOfPage = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
   var heightOfPage = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+  var leftOfPage = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
+  var widthOfPage = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
   var elY = 0
   var elH = 0
+  var elX = 0
+  var elW = 0
 
   if (document.layers) {
     elY = el.y
     elH = el.height
+    elX = el.x
+    elW = el.width
   } else {
     for (var p = el; p && p.tagName != 'BODY'; p = p.offsetParent) {
-      elY += p.offsetTop;
+      elY += p.offsetTop
+      elX += p.offsetLeft
     }
-    elH = el.offsetHeight;
+    elH = el.offsetHeight
+    elW = el.offsetWidth
   }
 
   if ((topOfPage + heightOfPage) < (elY + elH)) {
@@ -22,6 +30,16 @@ function scrollIntoViewIfOutOfView (el, options) {
       inline: 'nearest',
     }, options))
   } else if (elY < topOfPage) {
+    el.scrollIntoView(Object.assign({
+      block: 'start',
+      inline: 'nearest',
+    }, options))
+  } else if ((leftOfPage + widthOfPage) < (elX + elW)) {
+    el.scrollIntoView(Object.assign({
+      block: 'end',
+      inline: 'nearest',
+    }, options))
+  } else if (elX < leftOfPage) {
     el.scrollIntoView(Object.assign({
       block: 'start',
       inline: 'nearest',
